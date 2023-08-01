f94ec3dfbae70300414ec24cce28f0aa11415ee4
Jorge Saa <piojo.sistematico@gmail.com>
2023-07-31 20:42:26 -0400

    basic html

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import prevBtn from "./assets/images/icon-prev.svg";
+import nextBtn from "./assets/images/icon-next.svg";
+import quotes from "./assets/images/pattern-quotes.svg";
+import curve from "./assets/images/pattern-curve.svg";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((info) => setData(info));
+  }, []);
+
+  console.log(data);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <main>
+      <picture>
+        <img src={"../public/image-tanya.jpg"} alt="" />
+        <div>
+          <button>
+            <img src={prevBtn} alt="" />
+          </button>
+          <button>
+            <img src={nextBtn} alt="" />
+          </button>
+        </div>
+      </picture>
+      <section className="profile-section">
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          “ I’ve been interested in coding for a while but never taken the jump,
+          until now. I couldn’t recommend this course enough. I’m now in the job
+          of my dreams and so excited about the future. ”
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <span className="profile-name">Tanya Sinclair</span>
+        <span className="profile-title">UX Engineer</span>
+        <img src={quotes} alt="" />
+        <img src={curve} alt="" />
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
